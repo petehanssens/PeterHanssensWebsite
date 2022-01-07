55bdb43f1481fd4567964381c2d1d1822c148e9f
Peter Hanssens <phanssens1@gmail.com>
2022-01-07 11:50:55 +1100

    adding disqus (#42)

---
+++ components/disqus.js
@@ -0,0 +1,20 @@
+import {DiscussionEmbed} from "disqus-react"
+const DisqusComments = ( props ) => {
+  const post = props.props
+  console.log('post: ',post)
+  const disqusShortname = "peterhanssens"
+  const disqusConfig = {
+    url: "https://peterhanssens.com.au/blog/"+post.pageSlug,
+    identifier: post.pageSlug, // Single post id
+    title: post.title // Single post title
+  }
+  return (
+    <div>
+      <DiscussionEmbed
+        shortname={disqusShortname}
+        config={disqusConfig}
+      />
+    </div>
+  )
+}
+export default DisqusComments;
